f1b2d7fc635a13bbf10d37f2d3eb87b01c6b53b7
Lucas Carvalho <14770259794@cefet-rj.br>
2023-06-17 14:10:57 -0300

    && -> ||

--- src/main/java/me/lucthesloth/dominin/commands/tntonkill.java
+++ src/main/java/me/lucthesloth/dominin/commands/tntonkill.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 public class tntonkill implements CommandExecutor{
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (me.lucthesloth.dominin.Dominin.target == null && !me.lucthesloth.dominin.Dominin.target.isOnline()){
+        if (me.lucthesloth.dominin.Dominin.target == null || !me.lucthesloth.dominin.Dominin.target.isOnline()){
             sender.sendMessage("No target.");
             return false;
         }
